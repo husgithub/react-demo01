3cf2b23b74304c79fb2f46162898b0a32eab55df
husong <husong_163mailbox@163.com>
2019-01-26 14:59:34 +0800

    修改Home.js文件

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,7 +20,7 @@ class Home extends Component{
             <div>
                 <h2>Hello react !</h2>
                 <p>如果有多行代码必须return必须加()</p>
-                <p>并且所有html元素必须放在一个根节点下</p>
+                <p>并且所有html元素必须放在一个根节点下!</p>
                 <p>{this.state.msg}</p>
                 <p>{this.state.people.name}</p>
             </div>
